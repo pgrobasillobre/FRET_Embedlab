5a728fdb1b751b667d61b07fb29a6e635f1240e4
Pablo Grobas Illobre <pgrobasillobre@gmail.com>
2024-06-07 12:30:42 +0200

    setting up

--- src/python/field_on_xyz_grid/grid-field-creation-1-mol-around-Y/create_grid/1_create_vertical_grid/shift.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-counter=0
-for i in $(seq 14 -2.0 -16.0); do
-   python3 /Users/pablogrobasillobre/Dropbox/3rd-year/research/sveva-fret/programs/fret_embedlab_fortran/src/python/geom.py -t1 $counter  h1-up.xyz origin_CM_no -y
-   counter=$(echo "$counter + 2.0" | bc)
-done
