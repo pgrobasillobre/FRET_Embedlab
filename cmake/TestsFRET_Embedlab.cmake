d246e39f5f96f589be1c4087fda43afa7b403e43
Pablo Grobas Illobre <pgrobasillobre@gmail.com>
2025-05-22 16:22:05 +0200

    tests ok

--- cmake/TestsFRET_Embedlab.cmake
+++ cmake/TestsFRET_Embedlab.cmake
@@ -11,16 +11,11 @@ endmacro()
 
 # Add a keyword for the length of the test: 
 # 
-# 	short < 30 seconds
-# 	medium > 30 seconds < 120 seconds
-# 	long > 120 seconds < 200 seconds
-# 	verylong > 200 seconds
-# 
-# NEVER comment out tests
-add_FRET_Embedlab_runtest(integrate_density                     "FRET_Embedlab;density_integral")
-add_FRET_Embedlab_runtest(acceptor_donor_with_overlap_integral  "FRET_Embedlab;aceptor_donor_charges_overlap;")
-add_FRET_Embedlab_runtest(acceptor_donor_coulomb                "FRET_Embedlab;aceptor_donor_coulomb;")
-add_FRET_Embedlab_runtest(acceptor_np_charges                   "FRET_Embedlab;aceptor_np_charges;")
-
-add_FRET_Embedlab_runtest(acceptor_np_charges_donor_with_overlap_integral     "FRET_Embedlab;aceptor_np_donor_charges_overlap;")
+add_FRET_Embedlab_runtest(integrate_density                                "FRET_Embedlab;density_integral")
+add_FRET_Embedlab_runtest(acceptor_donor_with_overlap_integral             "FRET_Embedlab;aceptor_donor_charges_overlap;")
+add_FRET_Embedlab_runtest(acceptor_donor_coulomb                           "FRET_Embedlab;aceptor_donor_coulomb;")
+add_FRET_Embedlab_runtest(acceptor_np_charges                              "FRET_Embedlab;aceptor_np_charges;")
+add_FRET_Embedlab_runtest(acceptor_np_charges_dipoles_donor_coulomb        "FRET_Embedlab;acceptor_np_charges_dipoles_donor_coulomb;")
+add_FRET_Embedlab_runtest(acceptor_np_charges_donor_coulomb                "FRET_Embedlab;aceptor_np_charges_donor_coulomb;")
+add_FRET_Embedlab_runtest(acceptor_np_charges_donor_with_overlap_integral  "FRET_Embedlab;aceptor_np_donor_charges_overlap;")
 
